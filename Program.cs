cc586066ea127de3bcc3d8354dd5f20134f8ee22
daisykha <31575705+daisykha@users.noreply.github.com>
2022-02-08 14:17:41 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = 32;
+            Console.WindowWidth = 64;
+            int screenwidth = Console.WindowWidth;
+            int screenheight = Console.WindowHeight;
+        }
+    }
+}
